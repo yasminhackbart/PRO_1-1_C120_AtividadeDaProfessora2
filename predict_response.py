f8fe6059441bd83caaad5ed68604aa12ad7ef6bd
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2023-03-10 14:06:24 -0300

    Update predict_response.py

--- predict_response.py
+++ predict_response.py
@@ -57,12 +57,7 @@ def bot_response(user_input):
         bot_response = random.choice(intent['responses'])
         return bot_response
 
-print("Oi, eu sou a Estela, como posso ajudar?")
+    //crie o chatbot
 
-while True:
-    user_input = input("Digite sua mensagem aqui:")
-    print("Entrada do Usuário: ", user_input)
 
-    response = bot_response(user_input)
-    print("Resposta do Robô: ", response)
 
